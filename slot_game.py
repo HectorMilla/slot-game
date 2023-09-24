e8aca032b2bc91b9b11e852087ac494a7ebae773
Hector Milla <hectormilla8@gmail.com>
2023-09-24 18:08:20 -0400

    added replayability to game

--- slot_game.py
+++ slot_game.py
@@ -10,7 +10,22 @@ ROWS = 3
 COLS = 3
 
 symbol_count = {"A": 2, "B": 4, "C": 6, "D": 8}
+symbol_value = {"A": 5, "B": 4, "C": 3, "D": 2}
 
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+        return winnings, winning_lines
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
@@ -92,8 +107,7 @@ def get_bet():
     return amount
 
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -109,6 +123,20 @@ def main():
         f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}" )
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}.") 
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
 
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is: ${balance}")
+        answer = input("Enter W to play again or Q to quit")
+        if answer == "q":
+            break
+        balance += spin(balance)
+    print(f"You left with ${balance}")
 
 main()
